c8635f51fb04971337db06d01d6860f85b5d6da8
Kayque Amado <kayque_sfa@outlook.com>
2023-04-28 16:00:41 -0300

    📚Projeto Integrado

--- pagina/stop.js
+++ pagina/stop.js
@@ -1,28 +1,25 @@
-
-function enviarRequisicao() {
+function stopTransmition() {
         // Obter os valores dos campos de entrada
-        var tempo = document.getElementById("Tempo de Transmissão").value;
-        var frequencia = document.getElementById("freq").value;
-
+        var stop = "Stop";
+      
         // Criar um objeto FormData contendo os dados do formulário
         var formData = new FormData();
-        formData.append('tempo', tempo);
-        formData.append('frequencia', frequencia);
-
+        formData.append('stop', stop);
+      
         // Criar um objeto XMLHttpRequest
         var xhr = new XMLHttpRequest();
-
+      
         // Configurar a requisição HTTP POST
         var url = "192.168.4.213";// colocar ip do estátio do esp32
         xhr.open("get", url, true);
-
+      
         // Adicionar um listener para tratar a resposta da requisição
-        xhr.onreadystatechange = function () {
-                if (xhr.readyState == 4 && xhr.status == 200) {
-                        console.log(xhr.responseText);
-                }
+        xhr.onreadystatechange = function() {
+          if(xhr.readyState == 4 && xhr.status == 200) {
+            console.log(xhr.responseText);
+          }
         };
-
+      
         // Enviar a requisição com os dados do formulário
         xhr.send(formData);
-}
+      }
