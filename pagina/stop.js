3f5229f3fde6803be2a664bd1e346a23fd28aca1
Kayque Amado <kayque_sfa@outlook.com>
2023-05-02 16:46:09 -0300

    📚PI

--- pagina/stop.js
+++ pagina/stop.js
@@ -1,25 +1,15 @@
 function stopTransmition() {
-        // Obter os valores dos campos de entrada
-        var stop = "stop";
-      
-        // Criar um objeto FormData contendo os dados do formulário
-        var formData = new FormData();
-        formData.append('stop', stop);
-      
-        // Criar um objeto XMLHttpRequest
-        var xhr = new XMLHttpRequest();
-      
-        // Configurar a requisição HTTP POST
-        var url = "192.168.4.213";// colocar ip do estátio do esp32
-        xhr.open("get", url, true);
-      
-        // Adicionar um listener para tratar a resposta da requisição
-        xhr.onreadystatechange = function() {
-          if(xhr.readyState == 4 && xhr.status == 200) {
-            console.log(xhr.responseText);
-          }
-        };
-      
-        // Enviar a requisição com os dados do formulário
-        xhr.send(formData);
-      }
+  // 42 VAI SER O NUMERO QUE INDICA PARA PARAR A TRANSMISSÃO
+  var stop = 42;
+  console.log(stop);
+
+  const url = `192.168.4.213?${stop}`;
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      console.log(xhr.responseText);
+    }
+  };
+  xhr.send();
+}
